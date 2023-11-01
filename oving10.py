31a10a0b84f922b297ab44c2326dfbf5367e329f
simsle <144677608+simsle@users.noreply.github.com>
2023-11-01 17:28:01 +0100

    Add files via upload

---
+++ oving10.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 16:42:35 2023
+
+@author: slett
+"""
+
+########################################################################
+###                             Oving 10                             ###
+### Simon Slettevold, Silja Malin Vert Monig, Torstein Saltnes Olsen ###
+########################################################################
+
+# dd
